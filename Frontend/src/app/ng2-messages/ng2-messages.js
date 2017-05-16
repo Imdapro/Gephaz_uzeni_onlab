0b20aa1285b7df15204fd27d4a5ccbdac9d82d82
Unknown <andris@innosys.hu>
2017-05-16 21:35:05 +0200

    Backend - frontend integráció
    Docker, gitignore módosítás (fordított js fájlok kiszedése)

--- Frontend/src/app/ng2-messages/ng2-messages.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var ng2_messages_service_1 = require('./ng2-messages.service');
-exports.MessagesService = ng2_messages_service_1.MessagesService;
-var ng2_messages_component_1 = require('./ng2-messages.component');
-exports.MessagesComponent = ng2_messages_component_1.MessagesComponent;
-//# sourceMappingURL=ng2-messages.js.map
